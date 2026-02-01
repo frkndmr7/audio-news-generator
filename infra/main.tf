a0f11ee73cbda7d63597f1e88403e4ae1b41904e
Halil Furkan Damar <furkan@Halil-MacBook-Air.local>
2026-02-02 00:33:20 +0300

    fix: marketplace policy

--- infra/main.tf
+++ infra/main.tf
@@ -197,8 +197,15 @@ resource "aws_iam_role" "ecs_task_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
+      Action: [
+        "aws-marketplace:ViewSubscriptions",
+        "aws-marketplace:Subscribe"
+      ],
+      Effect: "Allow",
+      Resource: "*"
+
+      #Action = "sts:AssumeRole"
+      #Effect = "Allow"
       Principal = { Service = "ecs-tasks.amazonaws.com" }
     }]
   })
